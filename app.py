b7f8114c93aacf6e0619e7312b1a2e171ebbccc0
Tobi Ogunnaike <oluwatobi.ogunnaike@gmail.com>
2017-05-30 16:04:16 -0700

    added config.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from project import app
+import os
+
+
+if os.environ.get('ENV') == 'production':
+    app.config.from_object('config.ProductionConfig')
+else:
+    app.config.from_object('config.DevelopmentConfig')
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
